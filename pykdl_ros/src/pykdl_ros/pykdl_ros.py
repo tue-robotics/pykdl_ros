4302dfcf2884daf2750c09bf4e45becc9f00c4dc
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-08-10 21:53:26 +0200

    Change order of classes for sake of typehinting

--- pykdl_ros/src/pykdl_ros/pykdl_ros.py
+++ pykdl_ros/src/pykdl_ros/pykdl_ros.py
@@ -3,6 +3,73 @@ import PyKDL as kdl
 from std_msgs.msg import Header
 
 
+class FrameStamped:
+    """
+    Stamped version of PyKDL.Frame
+    """
+
+    def __init__(self, frame: kdl.Frame, stamp: Time, frame_id: str):
+        """
+        Constructor
+
+        :param frame: frame
+        :type frame: PykDL.Frame
+        :param stamp: TimeStamp
+        :type stamp: rospy.Time
+        :param frame_id: Frame ID
+        :type frame_id: str
+        """
+        assert isinstance(frame, kdl.Frame)
+        assert isinstance(stamp, Time)
+        assert isinstance(frame_id, str)
+        self.frame = frame
+        self.header = Header(frame_id=frame_id, stamp=stamp)
+
+    def __repr__(self):
+        xyz = "(x={x}, y={y}, z={z})".format(x=self.frame.p.x(), y=self.frame.p.y(), z=self.frame.p.z())
+        r, p, y = self.frame.M.GetRPY()
+        rpy = "(r={x}, p={y}, y={z})".format(x=r, y=p, z=y)
+        return "FrameStamped(pos:{pos}, rot:{rot} @ {fid})".format(pos=xyz, rot=rpy, fid=self.header.frame_id)
+
+    def __eq__(self, other):
+        if isinstance(other, FrameStamped):
+            return self.frame == other.frame and self.header.frame_id == other.header.frame_id
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    @staticmethod
+    def from_xyz_RPY(x: float, y: float, z: float, R: float, P: float, Y: float, stamp: Time, frame_id: str):
+        """
+        Custom constructor
+
+        :param x: x
+        :type x: float
+        :param y: y
+        :type y: float
+        :param z: z
+        :type z: float
+        :param R: Roll
+        :type R: float
+        :param P: Pitch
+        :type P: float
+        :param Y: Yaw
+        :type Y: float
+        :param stamp: TimeStamp
+        :type stamp: rospy.Time
+        :param frame_id: Frame ID
+        :type frame_id: str
+        :return: Filled object
+        :rtype: FrameStamped
+        """
+        vector = kdl.Vector(x, y, z)
+        rotation = kdl.Rotation(R, P, Y)
+        frame = kdl.Frame(rotation, vector)
+        return FrameStamped(frame, stamp, frame_id)
+
+
 class VectorStamped:
     """
     Stamped version of PyKDL.Vector
@@ -70,70 +137,3 @@ class VectorStamped:
         :rtype: VectorStamped
         """
         return VectorStamped(frame.frame.p, frame.header.stamp, frame.header.frame_id)
-
-
-class FrameStamped:
-    """
-    Stamped version of PyKDL.Frame
-    """
-
-    def __init__(self, frame: kdl.Frame, stamp: Time, frame_id: str):
-        """
-        Constructor
-
-        :param frame: frame
-        :type frame: PykDL.Frame
-        :param stamp: TimeStamp
-        :type stamp: rospy.Time
-        :param frame_id: Frame ID
-        :type frame_id: str
-        """
-        assert isinstance(frame, kdl.Frame)
-        assert isinstance(stamp, Time)
-        assert isinstance(frame_id, str)
-        self.frame = frame
-        self.header = Header(frame_id=frame_id, stamp=stamp)
-
-    def __repr__(self):
-        xyz = "(x={x}, y={y}, z={z})".format(x=self.frame.p.x(), y=self.frame.p.y(), z=self.frame.p.z())
-        r, p, y = self.frame.M.GetRPY()
-        rpy = "(r={x}, p={y}, y={z})".format(x=r, y=p, z=y)
-        return "FrameStamped(pos:{pos}, rot:{rot} @ {fid})".format(pos=xyz, rot=rpy, fid=self.header.frame_id)
-
-    def __eq__(self, other):
-        if isinstance(other, FrameStamped):
-            return self.frame == other.frame and self.header.frame_id == other.header.frame_id
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    @staticmethod
-    def from_xyz_RPY(x: float, y: float, z: float, R: float, P: float, Y: float, stamp: Time, frame_id: str):
-        """
-        Custom constructor
-
-        :param x: x
-        :type x: float
-        :param y: y
-        :type y: float
-        :param z: z
-        :type z: float
-        :param R: Roll
-        :type R: float
-        :param P: Pitch
-        :type P: float
-        :param Y: Yaw
-        :type Y: float
-        :param stamp: TimeStamp
-        :type stamp: rospy.Time
-        :param frame_id: Frame ID
-        :type frame_id: str
-        :return: Filled object
-        :rtype: VectorStamped
-        """
-        vector = kdl.Vector(x, y, z)
-        rotation = kdl.Rotation(R, P, Y)
-        frame = kdl.Frame(rotation, vector)
-        return FrameStamped(frame, stamp, frame_id)
