d271e3ba98f22b5e198fc83db0ed0df80a66361c
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2023-09-05 16:54:39 +0200

    (tf2_pykdl_ros) add Twist/WrenchStamped to integration test

--- tf2_pykdl_ros/test/rostest/test_tf2_ros_transform.py
+++ tf2_pykdl_ros/test/rostest/test_tf2_ros_transform.py
@@ -3,7 +3,7 @@
 import unittest
 
 import PyKDL as kdl
-from pykdl_ros import FrameStamped
+from pykdl_ros import VectorStamped, FrameStamped, TwistStamped, WrenchStamped
 import rospy
 
 import tf2_ros
@@ -18,23 +18,37 @@ from geometry_msgs.msg import TransformStamped
 
 
 class TestTransform(unittest.TestCase):
-    def test_transform(self):
-        buffer = tf2_ros.Buffer()
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.buffer = tf2_ros.Buffer()
         t = TransformStamped()
-        t.transform.translation.x = 1
-        t.transform.rotation.x = 1
+        t.transform.translation.x = 1.0
+        t.transform.rotation.x = 1.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 0.0
         t.header.stamp = rospy.Time(2)
         t.header.frame_id = "a"
         t.child_frame_id = "b"
-        buffer.set_transform(t, "test_tf2_ros_convert")
-        out = buffer.lookup_transform("a", "b", rospy.Time(2), rospy.Duration(2))
+        cls.buffer.set_transform(t, "test_tf2_ros_convert")
+
+    def test_lookup_transform(self):
+        out = self.buffer.lookup_transform("a", "b", rospy.Time(2), rospy.Duration(2))
         self.assertEqual(out.transform.translation.x, 1)
         self.assertEqual(out.transform.rotation.x, 1)
         self.assertEqual(out.header.frame_id, "a")
         self.assertEqual(out.child_frame_id, "b")
 
-        f = FrameStamped(kdl.Frame(kdl.Rotation.RPY(1, 2, 3), kdl.Vector(1, 2, 3)), rospy.Time(2), "a")
-        out = buffer.transform(f, "b")
+    def test_transform_vector(self):
+        v = kdl.Vector(1, 2, 3)
+        out: VectorStamped = self.buffer.transform(VectorStamped(v, rospy.Time(2), "a"), "b")
+        self.assertEqual(out.vector.x(), 0)
+        self.assertEqual(out.vector.y(), -2)
+        self.assertEqual(out.vector.z(), -3)
+
+    def test_transform_frame(self):
+        f = kdl.Frame(kdl.Rotation.RPY(1, 2, 3), kdl.Vector(1, 2, 3))
+        out: FrameStamped = self.buffer.transform(FrameStamped(f, rospy.Time(2), "a"), "b")
         self.assertEqual(out.frame.p.x(), 0)
         self.assertEqual(out.frame.p.y(), -2)
         self.assertEqual(out.frame.p.z(), -3)
@@ -42,6 +56,26 @@ class TestTransform(unittest.TestCase):
             out.frame.M.GetQuaternion(),
             (0.43595284407356577, -0.44443511344300074, 0.310622451065704, 0.7182870182434113),
         )
+
+    def test_transform_twist(self):
+        t = kdl.Twist(kdl.Vector(1, 2, 3), kdl.Vector(4, 5, 6))
+        out: TwistStamped = self.buffer.transform(TwistStamped(t, rospy.Time(2), "a"), "b")
+        self.assertEqual(out.twist.vel.x(), 1)
+        self.assertEqual(out.twist.vel.y(), -8)
+        self.assertEqual(out.twist.vel.z(), 2)
+        self.assertEqual(out.twist.rot.x(), 4)
+        self.assertEqual(out.twist.rot.y(), -5)
+        self.assertEqual(out.twist.rot.z(), -6)
+
+    def test_transform_wrench(self):
+        w = kdl.Wrench(kdl.Vector(1, 2, 3), kdl.Vector(4, 5, 6))
+        out: WrenchStamped = self.buffer.transform(WrenchStamped(w, rospy.Time(2), "a"), "b")
+        self.assertEqual(out.wrench.force.x(), 1)
+        self.assertEqual(out.wrench.force.y(), -2)
+        self.assertEqual(out.wrench.force.z(), -3)
+        self.assertEqual(out.wrench.torque.x(), 4)
+        self.assertEqual(out.wrench.torque.y(), -8)
+        self.assertEqual(out.wrench.torque.z(), -4)
 
 
 if __name__ == "__main__":
